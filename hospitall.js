4e39c33bbb84111e24b13f3d29a1e01110418d02
sidd5142 <siddharthyadav.5142@gmail.com>
2023-09-13 23:15:56 +0530

    Hospital Task

---
+++ hospitall.js
@@ -0,0 +1,80 @@
+var app = angular.module("myApp", ['ui.router']);
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) { 
+	$stateProvider
+		.state('Home', {
+            url: '/home',
+			templateUrl: 'home.html',
+			controller: 'HomeController'
+		})
+        .state('Register', {
+            url: '/register',
+			templateUrl: 'register.html',
+			controller: 'RegistrationController'
+		})
+
+		$urlRouterProvider.otherwise('/register');
+}]);
+
+app.controller('HomeController',function($scope,$http,$window,$state){
+
+});
+
+app.controller('RegistrationController',function($scope,$http,$window,$state){
+	$scope.Registrationfrom = function(){
+        console.log('FirstName :', $scope.firstname)
+		console.log('LstName :', $scope.lastname)
+		console.log('Username : ', $scope.username)
+		console.log('Email:', $scope.email)
+		console.log('Pass :', $scope.password)
+		console.log('ConfPass :', $scope.confpassword)
+		console.log('Contact :', $scope.contact)
+		console.log('Age :', $scope.age)
+		console.log('Address :', $scope.address)
+		console.log('Gender :', $scope.gender)
+	}
+
+	$scope.validatePassword = function(){
+		$scope.passwordMismatch = $scope.password !== $scope.confpassword;
+	  }
+
+
+	 $scope.register = function(){
+		var pass = $scope.password;
+		var confpass = $scope.confpassword;
+
+		var regdata = {
+            first_name: $scope.firstname,
+            last_name : $scope.lastname,
+            password : $scope.password,
+			email : $scope.email,
+			username : $scope.username,
+			age : $scope.age,
+			gender : $scope.gender,
+			contact : $scope.contact,
+			weight : $scope.weight,
+			address : $scope.address
+		};
+		console.log(regdata);
+
+		if(pass == confpass){
+			
+			$http.post('https://10.21.80.133:8000/api/PatientRegisteration/', regdata, {
+			headers: {'Content-Type': undefined},
+		    withCredentials: true
+		})
+          .then(function(response){
+            
+            console.log(response.data)
+			$state.go('Login');
+		  })
+		  .catch(function(error){
+			$window.alert(error);
+		  })
+		}
+		else{
+			$window.alert('Incorrect Password');
+			LoadingService.stopLoading();
+		}
+
+	 } 
+});
